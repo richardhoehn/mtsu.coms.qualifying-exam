cad0ab732a628b3e9be4db62142bdfc3e6ce4677
Richard Hoehn <rhoehn@gmail.com>
2023-07-17 17:02:00 -0500

    Update translate.py

--- tools/translate.py
+++ tools/translate.py
@@ -1,13 +1,11 @@
-
-
-
+# Using Deep Translator to leveage Google Translate
+# Link: https://cloud.google.com/translate/docs/reference/libraries/v2/python
 from deep_translator import GoogleTranslator
 
+sentence = 'Chocolate milk is so much better through a straw.'
 
-
-to_translate = 'This is a waste of my time!'
-translated = GoogleTranslator(source='auto', target='de').translate(to_translate)
-print(translated)
+translated = GoogleTranslator(source='auto', target='de').translate(sentence)
+print(translated) # Schokoladenmilch schmeckt durch einen Strohhalm viel besser.
 
 translated_back = GoogleTranslator(source='auto', target='en').translate(translated)
-print(translated_back)
+print(translated_back) # Chocolate milk tastes much better through a straw.
